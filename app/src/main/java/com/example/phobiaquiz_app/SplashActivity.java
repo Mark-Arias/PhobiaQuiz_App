3847b410a168ff5db223a0f35d191b5a96a03edd
marksarias <mark.arias7@gmail.com>
2020-07-01 10:35:36 -0700

    v5
    Tabled the item class for now. Have achieved main quiz functionality.
    Need to load in proper images.
    Need to load a quiz results activity and pass into it information about the test
    Encapsulate results calculation logic within that class
    ^^^
    Got the results activity working. Data is passed in and detected.
    
    Need to add in user login functionality
    Need to add in database storage of a user's results. Their name info plus their scored phobia
    Could load this db with some dummy data to generate some averages statistics for the user
    
    Admin login could give them access to all user data
    
    Splash window added

---
+++ app/src/main/java/com/example/phobiaquiz_app/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.phobiaquiz_app;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends Activity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_activity);
+
+        // Splash screen timer
+        int SPLASH_TIME_OUT = 6000;
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
